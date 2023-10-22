c6e32f5aea54923bc1cb3e6336347940da822dbe
SovietBeatle <kolby.ellis123@gmail.com>
2023-10-22 12:47:38 -0500

    Fix spacing on info-cards and made them appear vertical on small screens

--- src/components/Info.js
+++ src/components/Info.js
@@ -2,49 +2,67 @@ import React from 'react';
 import { Card } from 'react-bootstrap';
 
 function Info() {
-  return (
-    <div className=''>
-        <div className='test2'>
-        <h5>Plan your trip now</h5>
-        <h1>
-            Quick & easy car rental
-        </h1>
-        <div className='test3'>
-        <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="/operator.png" />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-      </Card.Body>
-    </Card>
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="/car-icon.png" />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-      </Card.Body>
-    </Card>
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="/van-icon.png" />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-      </Card.Body>
-    </Card>
-        </div>
-        </div>
-        
-    </div>
+  const cardImageStyle = {
+    display: 'block',
+    margin: '0 auto', // Center the image horizontally
+  };
 
+  const cardTextStyle = {
+    textAlign: 'center', // Center the text horizontally
+  };
+
+  const glowingImageStyle = {
+    filter: 'drop-shadow(0 0 10px #EF6A31)',
+  };
+
+  return (
+    <div className='info-container'>
+      <h5>Plan your trip now</h5>
+      <h1>Quick & easy car rental</h1>
+      <div className='info-card'>
+        <Card className="info-body" style={{ width: '18rem' }}>
+          <Card.Img
+            variant="top"
+            src="/operator.png"
+            style={{ ...cardImageStyle, width: '50px', height: '50px', ...glowingImageStyle }}
+          />
+          <Card.Body>
+            <Card.Title className="info-card-title" style={cardTextStyle}>Select Car</Card.Title>
+            <Card.Text className="info-text" style={cardTextStyle}>
+              We offer a long range of vehicles for all your driving needs. We have the perfect car to meet your needs.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+
+        <Card className="info-body" style={{ width: '18rem' }}>
+          <Card.Img
+            variant="top"
+            src="/car-icon.png"
+            style={{ ...cardImageStyle, width: '50px', height: '50px', ...glowingImageStyle }}
+          />
+          <Card.Body>
+            <Card.Title className="info-card-title" style={cardTextStyle}>Contact Operator</Card.Title>
+            <Card.Text className="info-text" style={cardTextStyle}>
+              Our knowledgeable and friendly operators are always ready to help with any questions or concerns.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+
+        <Card className="info-body" style={{ width: '18rem' }}>
+          <Card.Img
+            variant="top"
+            src="/van-icon.png"
+            style={{ ...cardImageStyle, width: '50px', height: '50px', ...glowingImageStyle }}
+          />
+          <Card.Body>
+            <Card.Title className="info-card-title" style={cardTextStyle}>Let's Drive</Card.Title>
+            <Card.Text className="info-text" style={cardTextStyle}>
+              Whether you're hitting the open road, we've got you covered with our wide range of cars.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </div>
+    </div>
   );
 }
 
