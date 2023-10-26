e69987f67a3ac75ee865d7cf69faebb977652d7b
SovietBeatle <kolby.ellis123@gmail.com>
2023-10-25 22:06:29 -0500

    Add team member cards in the Our Team page

--- src/pages/OurTeam.js
+++ src/pages/OurTeam.js
@@ -1,11 +1,85 @@
 import React from "react";
+import { Card, Col, Row, Container } from "react-bootstrap";
 
 function OurTeam() {
-    return (
-    <p>ourteam</p>
+  return (
+    <div className="team-container">
+      <h1>Our Team</h1>
 
+      <Container>
+        <Row>
+          <Col xs={12} sm={6} md={4} lg={4} xl={4}>
+            <Card className="team-card" style={{ height: "100%" }}>
+              <Card.Img className="team-pic" variant="top" src="/miller.png" />
+              <Card.Body>
+                <Card.Title>Luke Miller</Card.Title>
+                <Card.Text className="card-text">
+                  Salesman
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col xs={12} sm={6} md={4} lg={4} xl={4}>
+            <Card className="team-card" style={{ height: "100%" }}>
+              <Card.Img className="team-pic" variant="top" src="/diaz.png" />
+              <Card.Body>
+                <Card.Title>Michael Diaz</Card.Title>
+                <Card.Text className="card-text">
+                  Owner
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col xs={12} sm={6} md={4} lg={4} xl={4}>
+            <Card className="team-card" style={{ height: "100%" }}>
+              <Card.Img className="team-pic" variant="top" src="/ross.png" />
+              <Card.Body>
+                <Card.Title>Briana Ross</Card.Title>
+                <Card.Text className="card-text">
+                  Photographer
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          </Row>
+          <Row>
+          <Col xs={12} sm={6} md={4} lg={4} xl={4}>
+            <Card className="team-card" style={{ height: "100%" }}>
+              <Card.Img className="team-pic" variant="top" src="/rivera.png" />
+              <Card.Body>
+                <Card.Title>Lauren Rivera</Card.Title>
+                <Card.Text className="card-text">
+                  Car Detailist
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col xs={12} sm={6} md={4} lg={4} xl={4}>
+            <Card className="team-card" style={{ height: "100%" }}>
+              <Card.Img className="team-pic" variant="top" src="/rizz.png" />
+              <Card.Body>
+                <Card.Title>Martin Rizz</Card.Title>
+                <Card.Text className="card-text">
+                  Mechanic
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col xs={12} sm={6} md={4} lg={4} xl={4}>
+            <Card className="team-card" style={{ height: "100%" }}>
+              <Card.Img className="team-pic" variant="top" src="/hunt.png" />
+              <Card.Body>
+                <Card.Title>Caitlyn Hunt</Card.Title>
+                <Card.Text className="card-text">
+                  Manager
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+}
 
-
-        );
-    }
 export default OurTeam;
